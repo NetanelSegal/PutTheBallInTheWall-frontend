d995544459637600290f62426cdc53fea3040872
Netanel Segal <segal.netanel4@gmail.com>
2024-02-26 23:32:34 +0200

    simple tailwind styiling ……]((o_ _)'彡☆

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-blue-950 h-screen p-9">
+      <div className="relative w-full h-full outline outline-8  outline-white">
+        <div
+          className="w-20 h-20 rounded-full bg-slate-500"
+          id="playerOne"
+        ></div>
+        <div
+          className="w-20 h-20 rounded-full bg-slate-500"
+          id="playerTwo"
+        ></div>
+        <div
+          id="leftWall"
+          className="w-4 h-40 bg-black absolute z-10 top-1/2 -ml-4 -translate-y-1/2 left-0"
+        ></div>
+        <div
+          id="rightWall"
+          className="w-4 h-40 bg-black absolute z-10 top-1/2 -mr-4 -translate-y-1/2 right-0"
+        ></div>
+
+        <div
+          id="middleLine"
+          className="w-2 h-full bg-white absolute -translate-x-1/2 top-0 left-1/2"
+        ></div>
+
+        <div
+          id="middleCircle"
+          className="w-64 h-64 border-8 border-white rounded-full  absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+        ></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
