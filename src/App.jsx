70734e75fff003b815b2e2fbb632f21bd7e47935
Netanel Segal <segal.netanel4@gmail.com>
2024-03-04 22:30:34 +0200

    handleMousemove() prints touching when touching disc and isTouchingDisc()

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,9 @@
-import { useState, useEffect, useRef } from "react";
-
-const INITIAL_DISTANCE = 20;
-const SPEED_MULTIPLIER = 0.02;
+import { useRef } from "react";
+import Players from "./components/Players";
+import Disc from "./components/Disc";
 
 function App() {
-  const playerOneRef = useRef();
   const gameAreaRef = useRef();
-  const [playerOnePosition, setPlayerOnePosition] = useState({ x: 0, y: 0 });
-  const [playerOneDistance, setPlayerOneDistance] = useState(INITIAL_DISTANCE);
-  const [pressedKeys, setPressedKeys] = useState({});
-
-  const handleKeyDown = (e) => {
-    setPressedKeys((prev) => ({ ...prev, [e.key]: true }));
-  };
-
-  const handleKeyUp = (e) => {
-    setPressedKeys((prev) => ({ ...prev, [e.key]: false }));
-    setPlayerOneDistance(INITIAL_DISTANCE);
-  };
-
-  const movePlayer = () => {
-    let deltaX = 0;
-    let deltaY = 0;
-
-    if (pressedKeys["ArrowUp"]) {
-      deltaY -= playerOneDistance;
-    }
-    if (pressedKeys["ArrowDown"]) {
-      deltaY += playerOneDistance;
-    }
-    if (pressedKeys["ArrowRight"]) {
-      deltaX += playerOneDistance;
-    }
-    if (pressedKeys["ArrowLeft"]) {
-      deltaX -= playerOneDistance;
-    }
-
-    if (
-      playerOneRef.current.getBoundingClientRect().left <
-      gameAreaRef.current.getBoundingClientRect().left
-    ) {
-      setPlayerOnePosition((prev) => ({
-        ...prev,
-        x: gameAreaRef.current.getBoundingClientRect().left,
-        y: prev.y + deltaY,
-      }));
-    } else {
-      setPlayerOnePosition((prev) => ({
-        ...prev,
-        x: prev.x + deltaX,
-        y: prev.y + deltaY,
-      }));
-    }
-    setPlayerOneDistance((prev) => {
-      if (
-        pressedKeys["ArrowUp"] ||
-        pressedKeys["ArrowDown"] ||
-        pressedKeys["ArrowRight"] ||
-        pressedKeys["ArrowLeft"]
-      ) {
-        const newDistance = prev + prev * SPEED_MULTIPLIER;
-        return Math.min(newDistance, INITIAL_DISTANCE * 100); // Limiting maximum speed
-      }
-      return prev;
-    });
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      return Object.values(pressedKeys).includes(true) && movePlayer();
-    }, 16); // Run the movePlayer function approximately every 16ms (60fps)
-    return () => clearInterval(interval);
-  }, [pressedKeys]);
-
-  useEffect(() => {
-    console.log(playerOnePosition);
-    if (
-      playerOneRef.current.getBoundingClientRect().left <
-      gameAreaRef.current.getBoundingClientRect().left
-    ) {
-      playerOneRef.current.style.transform = `translate(${
-        gameAreaRef.current.getBoundingClientRect().left
-      }px, ${playerOnePosition.y}px)`;
-    } else
-      playerOneRef.current.style.transform = `translate(${playerOnePosition.x}px, ${playerOnePosition.y}px)`;
-  }, [playerOnePosition]);
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
-    document.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-      document.removeEventListener("keyup", handleKeyUp);
-    };
-  }, []);
 
   return (
     <div className="bg-blue-950 h-screen p-9">
@@ -102,31 +11,8 @@ function App() {
         ref={gameAreaRef}
         className="relative w-full h-full outline outline-8  outline-white"
       >
-        <div
-          ref={playerOneRef}
-          className="absolute top-0 left-0 w-20 h-20 rounded-full bg-slate-500 transition-all duration-0 ease-in-out"
-          id="playerOne"
-        ></div>
-        <div
-          className="w-20 h-20 rounded-full bg-slate-500"
-          id="playerTwo"
-        ></div>
-        <div
-          id="leftWall"
-          className="w-4 h-40 bg-black absolute z-10 top-1/2 -ml-4 -translate-y-1/2 left-0"
-        ></div>
-        <div
-          id="rightWall"
-          className="w-4 h-40 bg-black absolute z-10 top-1/2 -mr-4 -translate-y-1/2 right-0"
-        ></div>
-        <div
-          id="middleLine"
-          className="w-2 h-full bg-white absolute -translate-x-1/2 top-0 left-1/2"
-        ></div>
-        <div
-          id="middleCircle"
-          className="w-64 h-64 border-8 border-white rounded-full  absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-        ></div>
+        <Disc />
+        <Players gameAreaRef={gameAreaRef} />
       </div>
     </div>
   );
---
+++ src/components/Disc.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Disc = () => {
+  const ref = useRef();
+  const [isTouching, setIsTouching] = useState(false);
+  const [speed, setSpeed] = useState(0);
+
+  const handleMousemove = (e) => {
+    const mousePos = { x: e.clientX, y: e.clientY };
+    const discPos = ref.current.getBoundingClientRect();
+
+    const newSpeed = calculateSpeed(
+      mousePos.x,
+      mousePos.y,
+      discPos.x + discPos.width / 2,
+      discPos.y + discPos.height / 2,
+      Date.now()
+    );
+
+    setSpeed(newSpeed);
+
+    if (isTouchingDisc(mousePos, discPos)) {
+      setIsTouching(true);
+    } else {
+      setIsTouching(false);
+    }
+  };
+
+  const isTouchingDisc = (mousePosition, discPosition) => {
+    const discCenter = {
+      x: discPosition.left + discPosition.width / 2,
+      y: discPosition.top + discPosition.height / 2,
+    };
+
+    const distance = Math.sqrt(
+      Math.pow(mousePosition.x - discCenter.x, 2) +
+        Math.pow(mousePosition.y - discCenter.y, 2)
+    );
+
+    return distance <= discPosition.width / 2;
+  };
+
+  useEffect(() => {
+    if (isTouching) {
+      console.log(speed);
+    }
+  }, [speed, isTouching]);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMousemove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMousemove);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={ref}
+      className="w-20 z-30 rounded-full h-20 absolute bg-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+    ></div>
+  );
+};
+
+function calculateSpeed(currentX, currentY, lastX, lastY, lastTimestamp) {
+  const deltaTime = Date.now() - lastTimestamp;
+  if (deltaTime > 0) {
+    const distance = Math.sqrt(
+      Math.pow(currentX - lastX, 2) + Math.pow(currentY - lastY, 2)
+    );
+    return distance / deltaTime;
+  }
+  return 0;
+}
+
+export default Disc;
